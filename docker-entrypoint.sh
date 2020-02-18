1b742638ddfd05a1c8a6846207347ff3e824cda5
amazingguni <amazingguni@gmail.com>
2020-02-18 16:03:41 +0900

    Add makemigrations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,7 @@ echo "Collect static files"
 python manage.py collectstatic --noinput
 
 echo "Apply database migrations"
+python manage.py makemigrations
 python manage.py migrate
 
 echo "Create admin user"
