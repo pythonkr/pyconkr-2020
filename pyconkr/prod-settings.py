afdb82ed9edbee029731650846d77d93a751acb1
amazingguni <amazingguni@gmail.com>
2020-02-18 15:54:36 +0900

    Set debug True for debugging

--- pyconkr/prod-settings.py
+++ pyconkr/prod-settings.py
@@ -1,7 +1,10 @@
 from pyconkr.settings import *
 
-DEBUG = False
+DEBUG = True
+# For debugging
 ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
     'www.pycon.kr',
     'dev.pycon.kr',
     'pycon.kr'
