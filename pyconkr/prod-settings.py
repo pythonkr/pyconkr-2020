7fbaff39db97cb33552b64b4adc33b3aec0278f0
amazingguni <amazingguni@gmail.com>
2020-02-24 13:16:56 +0900

    Add ELB healthcheck hostname into allowed host

--- pyconkr/prod-settings.py
+++ pyconkr/prod-settings.py
@@ -1,13 +1,20 @@
 from pyconkr.settings import *
+import os
 
 DEBUG = False
-# For debugging
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     'www.pycon.kr',
     'dev.pycon.kr',
     'pycon.kr',
-    '*'
 ]
 
+# Append ELB healthcheck hostname(internal ip address)
+# https://stackoverflow.com/questions/55718292/getting-400s-from-aws-elb-hostcheck-to-work-with-django-allowed-hosts-in-aws-ec
+if 'ECS_CONTAINER_METADATA_URI' in os.environ:
+    ELB_HEALTHCHECK_HOSTNAMES = [ip for network in
+                                 requests.get(os.environ['ECS_CONTAINER_METADATA_URI']).json()[
+                                     'Networks']
+                                 for ip in network['IPv4Addresses']]
+    ALLOWED_HOSTS += ELB_HEALTHCHECK_HOSTNAMES
