731c2e3bbf43cbad48b3925c8cfe0a21ee90341b
amazingguni <amazingguni@gmail.com>
2020-02-28 17:55:21 +0900

    Use collectfast to be faster collectstatic command

--- pyconkr/prod-settings.py
+++ pyconkr/prod-settings.py
@@ -52,9 +52,14 @@ for key in aws_env_keys:
         print(f'You should set {key}')
         exit(1)
 
+INSTALLED_APPS += (
+    'collectfast',
+)
+
 AWS_DEFAULT_ACL = 'public-read'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
