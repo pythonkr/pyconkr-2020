1b71755afbad52452fd0b2ec9ee7d0624e91d15b
amazingguni <amazingguni@gmail.com>
2020-03-17 17:36:51 +0900

    Change sponsor join view to update view

--- sponsor/urls.py
+++ sponsor/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from sponsor.views import SponsorList, SponsorDetail, SponsorCreate
+from sponsor.views import SponsorList, SponsorDetail, SponsorUpdate
 
 
 from django.contrib import admin
@@ -8,8 +8,8 @@ admin.autodiscover()
 urlpatterns = [
     path('list/',
          SponsorList.as_view(), name='sponsors'),
-    path('detail/<slug:slug>/',
+    path('detail/<int:pk>/',
          SponsorDetail.as_view(), name='sponsor'),
     path('join/',
-         SponsorCreate.as_view(), name='join_sponsor'),
+         SponsorUpdate.as_view(), name='join_sponsor'),
 ]
