39f906d1d2707480f489e02b96865ce21aa972bf
Hyeok Min Kwon <khmzxc@naver.com>
2020-03-31 20:22:29 +0900

    Hide venue page

--- pyconkr/context_processors.py
+++ pyconkr/context_processors.py
@@ -26,7 +26,7 @@ def default(request):
                 'title': _('About'),
                 'submenu': OrderedDict([
                     ('pyconkr', {'title': _('About PyCon Korea 2020')}),
-                    ('venue', {'title': _('Venue')}),
+                    # ('venue', {'title': _('Venue')}),
                     ('schedule', {'title': _('Important Dates')}),
                     ('patron', {'title': _('Patrons')}),
                     ('coc', {'title': _('Code of Conduct')}),
