14f0f4dddce89c2baa2a6a3ebbec30b8cbc961da
Hyeok Min Kwon <khmzxc@naver.com>
2020-05-27 21:57:22 +0900

    Revert "SCSS 추가"
    This reverts commit 74d5aa09a8705fe02c3ffefcd7a95b7963f860f1.

--- pyconkr/settings.py
+++ pyconkr/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = (
     'django_csv_exports',
     'mail_templated',
     'import_export',
-    'sass_processor',
 ) + (
     # local apps
     'pyconkr',
@@ -346,19 +345,3 @@ LOGGING = {
         },
     }
 }
-
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'sass_processor.finders.CssFinder',
-]
-
-SASS_PROCESSOR_AUTO_INCLUDE = False
-
-SASS_PROCESSOR_INCLUDE_FILE_PATTERN = r'^.+\.scss$'
-
-SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
-
-COMPRESS_PRECOMPILERS = (
-    ('text/x-scss', 'django_libsass.SassCompiler'),
-)
