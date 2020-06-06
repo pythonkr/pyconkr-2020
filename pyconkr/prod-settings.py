effbaf6d78ec95b1d410fa2e549f5a8b1cf7f528
ParkSeongHeum <ab6449@gmail.com>
2020-06-06 16:55:02 +0900

    AWS S3에 STATIC 파일을 저장하도록 설정 수정 및 테스트를 위해 STATIC에 SCSS 중복 설정

--- pyconkr/prod-settings.py
+++ pyconkr/prod-settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS += (
 
 AWS_DEFAULT_ACL = 'public-read'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'sass_processor.storage.SassS3Boto3Storage'
 COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
@@ -49,4 +50,4 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 COMPRESS_OFFLINE = True
 LIBSASS_OUTPUT_STYLE = 'compressed'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
