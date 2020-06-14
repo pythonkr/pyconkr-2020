2e7434030095e37b7fb231ccf21328fc282af19d
ParkSeongHeum <ab6449@gmail.com>
2020-06-14 18:45:25 +0900

    Revert "Remove categories(WIP) 복원"
    This reverts commit 0895a03dcf5228700e9406e72d1050ed7378ce40.

--- program/admin.py
+++ program/admin.py
@@ -77,7 +77,7 @@ class ProposalAdminForm(forms.ModelForm):
 
 class ProposalAdmin(admin.ModelAdmin):
     form = ProposalAdminForm
-    list_display = ('id', 'user', 'title', 'difficulty', 'duration', 'language', 'category')
+    list_display = ('id', 'user', 'title', 'difficulty', 'duration', 'language')
 
 
 admin.site.register(Proposal, ProposalAdmin)
