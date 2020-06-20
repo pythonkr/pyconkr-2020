3d04372e9310c00107460e6a2ab8a13221601729
Hyeok Min Kwon <khmzxc@naver.com>
2020-06-20 16:14:52 +0900

    Edit sponsor detail url

--- sponsor/urls.py
+++ sponsor/urls.py
@@ -8,12 +8,12 @@ admin.autodiscover()
 urlpatterns = [
     path('list/',
          SponsorList.as_view(), name='sponsors'),
-    path('detail/<int:pk>/',
-         SponsorDetail.as_view(), name='sponsor'),
+    re_path(r'^detail/(?P<slug>\w+)/$',
+            SponsorDetail.as_view(), name='sponsor'),
     path('join/',
          SponsorUpdate.as_view(), name='join_sponsor'),
     path('virtual_booth/',
          VirtualBooth.as_view(), name='virtual_booth_home'),
-    re_path(r'^virtual_booth/(?P<slug>\w+)$',
+    re_path(r'^virtual_booth/(?P<slug>\w+)/$',
             VirtualBoothDetail.as_view(), name='virtual_booth')
 ]
