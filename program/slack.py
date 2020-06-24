4b1558908c784e3f243d53cfd434977f3c613061
ParkSeongHeum <ab6449@gmail.com>
2020-06-24 23:01:11 +0900

    CFS 슬랙 메시지 수정

--- program/slack.py
+++ program/slack.py
@@ -57,7 +57,7 @@ def new_cfs_registered(hostname, pk, title):
         attachment = {
             "color": 'good',
             "title": '새로운 CFS가 등록되었습니다. :)',
-            "text": '제목: {}\n 주소: {}'.format(title, url),
+            "text": '스폰서 이름: {}\n 주소: {}'.format(title, url),
         }
 
         slack.chat.post_message(
@@ -69,14 +69,14 @@ def cfs_updated(hostname, pk, title):
     if token:
         slack = Slacker(token)
         # color: danger(red), good(green)
-        text = '안녕, 나는 알려주길 좋아하는 CFP-BOT! CFS에 바뀐게 있어 AWS에서 따라왔지!'
+        text = '안녕, 나는 알려주길 좋아하는 CFP-BOT! 신청한 스폰서 내용 중 바뀐게 있어 AWS에서 따라왔지!'
         # url = URL_TEMPLATE.format(pk)
         url = hostname + reverse('admin:sponsor_sponsor_change', args=(pk,))
 
         attachment = {
             "color": 'good',
             "title": '수정된 CFS가 있습니다. :)',
-            "text": '제목: {}\n 주소: {}'.format(title, url),
+            "text": '스폰서 이름: {}\n 주소: {}'.format(title, url),
         }
 
         slack.chat.post_message(
