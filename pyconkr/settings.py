dc422b95ac688634f06b8131a399c48b2444c1e4
ParkSeongHeum <ab6449@gmail.com>
2020-07-01 22:51:40 +0900

    Session Remember 옵션 추가

--- pyconkr/settings.py
+++ pyconkr/settings.py
@@ -205,6 +205,7 @@ SOCIALACCOUNT_ADAPTER = 'user.adapter.SocialAdapter'
 SOCIALACCOUNT_FORMS = {
     'signup': 'user.forms.SocialSignupForm'
 }
+ACCOUNT_SESSION_REMEMBER  = True
 
 if os.getenv('EMAIL_HOST_USER'):
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
