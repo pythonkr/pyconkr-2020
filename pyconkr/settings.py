797feefed9e4fc3e20b03cd96fbb0e3d4fd0e799
Parkseongheum <ab6449@gmail.com>
2020-07-05 18:10:38 +0900

    이메일 발송 관련 설정 추가

--- pyconkr/settings.py
+++ pyconkr/settings.py
@@ -370,3 +370,14 @@ COMPRESS_PRECOMPILERS = (
     ('text/x-scss', 'django_libsass.SassCompiler'),
 )
 
+# MAILING
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+# EMAIL_HOST_USER = constance.config.EMAIL_HOST_USER
+# EMAIL_HOST_PASSWORD = constance.config.EMAIL_HOST_PASSWORD
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = constance.config.EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
