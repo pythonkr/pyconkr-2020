7f7ffa538906182bb2ef9712359b4d7bbc55d2e8
Parkseongheum <ab6449@gmail.com>
2020-08-29 03:24:50 +0900

    티켓 구매시 메일 발송 기능 추가

---
+++ mailing/helper.py
@@ -0,0 +1,32 @@
+from django.core.mail import send_mail, EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+from user.models import Profile
+
+
+def send_ticket(orm_profile: Profile):
+    title = 'PyCon KR 2020에 등록해주셔서 감사합니다.'
+    html_content = render_to_string('email_ticket.html', {'profile': orm_profile})
+    sender_name = 'PyCon Korea'
+
+    email = EmailMultiAlternatives(
+        title,
+        html_content,
+        'PyCon Korea',
+        [orm_profile.user.email],
+    )
+
+    email.attach_alternative(html_content, "text/html")
+    email.content_subtype = 'html'  # set the primary content to be text/html
+    email.mixed_subtype = 'related'  # it is an important part that ensures embedding of an image
+
+    email.send(fail_silently=False)
+
+    # send_mail(
+    #     title,
+    #     html_content,
+    #     'PyCon Korea',
+    #     [orm_profile.user.email],
+    #     fail_silently=False,
+    #     html_message=html_content
+    # )
--- registration/signal.py
+++ registration/signal.py
@@ -1,10 +1,17 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from mailing.helper import send_ticket
+from user.models import Profile
+from registration.models import Ticket
 
-@receiver(post_save)
-def send_ticket_info(sender, **kwargs):
+
+@receiver(post_save, sender=Ticket)
+def send_ticket_info(sender, instance, **kwargs):
     print("sender: ", sender)
+    print("instance: ", instance)
     print("kwargs: ", kwargs)
-    
-    # TODO: 티켓 메일발송
+
+    user_profile = Profile.objects.get(user=instance.user)
+
+    send_ticket(user_profile)
