5d4641f86a971061edfb01eb3ff89866759d11e8
Jake Tae <jaesungtae@gmail.com>
2020-08-29 19:03:05 +0900

    fix: use correct imports
    This PR removes adds necessary imports that were previously missing.

--- announcement/views.py
+++ announcement/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from datetime import datetime
+
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 
 from .models import Announcement
@@ -8,9 +10,8 @@ class AnnouncementList(ListView):
     model = Announcement
 
     def get_queryset(self):
-        now = datetime.now()
         queryset = super(AnnouncementList, self).get_queryset()
-        return queryset.filter(Q(announce_after__isnull=True) | Q(announce_after__lt=now))
+        return queryset.filter(Q(announce_after__isnull=True) | Q(announce_after__lt=datetime.now()))
 
 
 class AnnouncementDetail(DetailView):
