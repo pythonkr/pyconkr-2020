df8b575db9643e5fa9d9995df74e5907442e9808
Hyeok Min Kwon <khmzxc@naver.com>
2020-08-30 14:42:34 +0900

    Add action in registration admin

--- registration/admin.py
+++ registration/admin.py
@@ -9,12 +9,17 @@ class TicketAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_filter = ('is_patron', 'agree_coc',)
     search_fields = ('user__profile__user_code', 'user__email',)
     autocomplete_fields = ('user',)
-    actions = ('to_patron',)
+    actions = ('to_patron', 'to_agree_coc',)
 
     def to_patron(self, request, queryset):
         queryset.update(is_patron=True)
 
     to_patron.short_description = "개인 후원으로 지정합니다."
+
+    def to_agree_coc(self, request, queryset):
+        queryset.update(agree_coc=True)
+
+    to_agree_coc.short_description = "CoC에 동의한 것으로 합니다."
 
     def get_user_code(self, obj):
         return obj.user.profile.user_code
