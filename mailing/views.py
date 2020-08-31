593ef609a2b4d2001f7a7a802cbb63997557c3f5
Hyeok Min Kwon <khmzxc@naver.com>
2020-08-31 14:32:37 +0900

    Template 위치 변경

--- mailing/views.py
+++ mailing/views.py
@@ -7,7 +7,7 @@ from .forms import NewsLetterAddForm
 
 class NewsLetterAdd(CreateView):
     form_class = NewsLetterAddForm
-    template_name = "pyconkr/newsletter_add.html"
+    template_name = "newsletter_add.html"
 
     def get(self, request, *args, **kwargs):
         return super(NewsLetterAdd, self).get(request, *args, **kwargs)
