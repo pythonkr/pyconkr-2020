644f0dfa313f917ce0f4c94aa043f23278d489bd
Hyeok Min Kwon <khmzxc@naver.com>
2020-09-03 22:03:22 +0900

    모든 urls의 import 수정, 규칙 통일

--- sponsor/urls.py
+++ sponsor/urls.py
@@ -1,28 +1,19 @@
-from django.urls import path, re_path
-from django.contrib.auth.decorators import login_required
-from sponsor.views import SponsorCreate, SponsorDetail, SponsorProposalHome, SponsorProposalDetail, SponsorUpdate, \
-    VirtualBooth, VirtualBoothDetail, VirtualBoothUpdate, LoginForSponsor
-
-
+from django.urls import re_path
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
+
+from . import views
+
 admin.autodiscover()
 
 urlpatterns = [
-    path('join/home/',
-         SponsorProposalHome.as_view(), name='sponsor_propose_home'),
-    path('join/form/',
-         login_required(SponsorCreate.as_view()), name='sponsor_propose'),
-    path('join/detail/',
-         SponsorProposalDetail.as_view(), name='sponsor_proposal_detail'),
-    path('join/edit/',
-         SponsorUpdate.as_view(), name='sponsor_proposal_edit'),
-    re_path(r'^detail/(?P<slug>\w+)/$',
-            SponsorDetail.as_view(), name='sponsor_detail'),
-    path('virtual_booth/',
-         VirtualBooth.as_view(), name='virtual_booth_home'),
-    re_path(r'^virtual_booth/(?P<slug>\w+)/$',
-            VirtualBoothDetail.as_view(), name='virtual_booth'),
-    re_path(r'^virtual_booth/(?P<slug>\w+)/edit/',
-            VirtualBoothUpdate.as_view(), name='virtual_booth_update'),
-    path('login/', LoginForSponsor.as_view(), name='login_for_sponsor'),
+    re_path(r'^join/home/$', views.SponsorProposalHome.as_view(), name='sponsor_propose_home'),
+    re_path(r'^join/form/$', login_required(views.SponsorCreate.as_view()), name='sponsor_propose'),
+    re_path(r'^join/detail/$', views.SponsorProposalDetail.as_view(), name='sponsor_proposal_detail'),
+    re_path(r'^join/edit/$', views.SponsorUpdate.as_view(), name='sponsor_proposal_edit'),
+    re_path(r'^detail/(?P<slug>\w+)$', views.SponsorDetail.as_view(), name='sponsor_detail'),
+    re_path(r'^virtual_booth/$', views.VirtualBooth.as_view(), name='virtual_booth_home'),
+    re_path(r'^virtual_booth/(?P<slug>\w+)$', views.VirtualBoothDetail.as_view(), name='virtual_booth'),
+    re_path(r'^virtual_booth/(?P<slug>\w+)/edit/$', views.VirtualBoothUpdate.as_view(), name='virtual_booth_update'),
+    re_path(r'^login/$', views.LoginForSponsor.as_view(), name='login_for_sponsor'),
 ]
--- user/urls.py
+++ user/urls.py
@@ -2,13 +2,13 @@ from django.contrib.auth.decorators import login_required
 from django.urls import re_path
 from user.views import ProfileDetail, ProfileUpdate
 
-
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = [
     re_path(r'^$',
             login_required(ProfileDetail.as_view()), name='profile'),
-    re_path(r'^edit$',
+    re_path(r'^edit/$',
             login_required(ProfileUpdate.as_view()), name='profile_edit'),
 ]
