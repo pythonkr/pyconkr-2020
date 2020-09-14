35904f59ae6093278c85d3245bb9896a2a5f17bc
Hyeok Min Kwon <khmzxc@naver.com>
2020-09-14 21:13:43 +0900

    후원자 목록, 시간표 cache 사용하게

--- program/urls.py
+++ program/urls.py
@@ -4,6 +4,7 @@ from django.urls import re_path
 from . import views
 
 from django.contrib import admin
+from django.views.decorators.cache import cache_page
 
 admin.autodiscover()
 
@@ -11,7 +12,7 @@ urlpatterns = [
     re_path(r'^talks/$', views.ProgramList.as_view(), name='talk-list'),
     re_path(r'^talk/(?P<pk>\d+)$', views.ProgramDetail.as_view(), name='talk'),
     re_path(r'^talk/(?P<pk>\d+)/edit/$', login_required(views.ProgramUpdate.as_view()), name='talk-update'),
-    re_path(r'^talk-schedule/$', views.ProgramSchedule.as_view(), name='talk-schedule'),
+    re_path(r'^talk-schedule/$', cache_page(60 * 60)(views.ProgramSchedule.as_view()), name='talk-schedule'),
     re_path(r'^sprint/$', views.SprintList.as_view(), name='sprint'),
     re_path(r'^keynote/$', views.KeynoteList.as_view(), name='keynote'),
     re_path(r'^cfp/propose/$', login_required(views.ProposalCreate.as_view()), name='propose'),
