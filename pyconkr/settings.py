eb76138b1564a4e282c733f15567f6c98886c65a
constmoon <jiwoo.link@gmail.com>
2021-09-28 10:31:40 +0900

    미들웨어 X-Frame-Options 제거

--- pyconkr/settings.py
+++ pyconkr/settings.py
@@ -74,7 +74,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
